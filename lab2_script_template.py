cceb776beb041f93c515c7c4654e7537a17abda9
Yuvraj3k <Yuvraj3k@users.noreply.github.com>
2024-01-24 10:45:13 -0500

    Step 4 done

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -14,14 +14,20 @@ def main():
              "title": "brightburn",
              "genre": "horrer"   
             },
-        ]
+        ],
         
     }
 
     # TODO: Step 3 - Add another movie to the data structure
-    
+    about_me["Movies"].append({"title": "life","genre":"sci-fi"})
+    print_student_name_and_id(about_me)
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
+    full_name= about_me["full_name"]
+    first_name= full_name.split()[0]
+    student_id= about_me["Student_id"]
+    full_name= about_me["Student_id"]
+    print(f"My name is {full_name}, but you can call me Sir {first_name}.\nMy student ID is {student_id}.")
     return
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
